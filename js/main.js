e3efbefc52dc933a7e0c1cd092a046430d2d74fa
pacimarco <m.paci1987@gmail.com>
2022-07-14 12:39:12 +0200

    fix <= in ciclo for inner html vuoto per svuotare grid

--- js/main.js
+++ js/main.js
@@ -1,119 +1,57 @@
-const gridDom = document.getElementById('grid');
+const playDom = document.getElementById('play');
+playDom.addEventListener('click', inizioGioco );
 
+function inizioGioco() {
+    const difficult=parseInt(document.getElementById('difficult').value);
 
+    let cellTotal;
+    let cellPerSide;
 
-
-for (let i = 1; i <=100; i++) {
-    
-    const currentElement = createGridSquare();
-
-    
-
-    
-
-    currentElement.addEventListener('click', 
-        function() {
-            currentElement.classList.toggle('clicked');
-            
-            console.log(i);
-        }
-        
-    );
-          
-        gridDom.append(currentElement);
+    switch (difficult) {
+        case 1:
+            default:
+                cellTotal = 100;
+                cellPerSide = 10;
+                break;
+        case 2:
+            cellTotal = 81;
+            cellPerSide = 9;
+            break;
+        case 3:
+            cellTotal = 49;
+            cellPerSide = 7;
+            break;
 }
 
-function createGridSquare() {
-    
-    const currentElement = document.createElement('div');
-    currentElement.classList.add('square');
+generatePlayground();
 
-    return currentElement;
-}
-/* numerazione progressiva da 1 a 100 */
-function getNumber (num) {
-    for (let i = 1; i <= 100; i++) {
-        const currentElement = createGridSquare();
-        gridDom.append(currentElement);
-    }
-    console.log(i);
-}
+    function generatePlayground() {
+        const gridDom = document.getElementById('grid');
 
+        gridDom.innerHTML = ''; // svuota il playground
 
+        for (let i = 1; i <= cellTotal; i++) {  //parto da 1
 
+            const currentElement = createGridSquare(i, cellPerSide);
+            currentElement.addEventListener('click',
+                function () {
+                    console.log(this.innerText);
+                    this.classList.toggle('clicked');
+                }
+            );
 
-
-
-
-
-
-/*
-const gridDom = document.getElementById('grid');
-
-const numUsati = [];
-
-
-for (let i = 1; i <=100; i++) {
-    
-    const numeroProgressivo = getUniqueNumber(numUsati,1,100);
-    numUsati.push(numeroProgressivo);
-
-    const currentElement = createGridSquare(numeroProgressivo);
-
-    
-
-    
-
-    currentElement.addEventListener('click', 
-        function() {
-            currentElement.classList.toggle('clicked');
-            
-            
-        }
-        
-    );
-          
-        gridDom.append(currentElement);
-}
-
-
-
-function getNumeroProgressivo (min, max) {
-    for (let i = 1; i <= 100; i++) {
-        
-        if (i >= min && i <= max) {
-            return i;
-        }
-    }
-    //return numeroProgressivo;
-}
-
-function getUniqueNumber(array, min, max) {
-    let numeroValido = false;
-    let numeroCreato;
-    while ( numeroValido === false) {
-        numeroCreato = getNumeroProgressivo(min, max);
-        if (array.includes(numeroCreato) === false) {
-            numeroValido = true;
+            gridDom.appendChild(currentElement);
         }
     }
 
-    return numeroCreato;
+    function createGridSquare(numero, cellPerSide) {
+        
+        const currentElement = document.createElement('div');
+        currentElement.classList.add('square');
+        currentElement.append(numero);
+        currentElement.style.width = `${100 / cellPerSide}%`;
+        currentElement.style.height = `${100 / cellPerSide}%`;
+        
+        return currentElement;
+    }
 }
-
-function createGridSquare(numero) {
-    
-    const currentElement = document.createElement('div');
-    currentElement.classList.add('square');
-
-    const numberSquare = document.createElement('div');
-    numberSquare.classList.add('square-number');
-    numberSquare.innerHTML = numero;
-    currentElement.append(numberSquare);
-
-    return currentElement;
-}
-
-non risponde in browser..gira all'infinito
-dopo aver corretto gli errori
-*/ 
